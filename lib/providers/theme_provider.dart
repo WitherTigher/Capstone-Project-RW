08b0f8ce48221a7ad2a6d48180c211117f5e15f7
Max Koon <22125083+k2on@users.noreply.github.com>
2025-12-01 21:54:20 -0500

    feat: persist theme

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.light;
@@ -6,8 +7,30 @@ class ThemeProvider with ChangeNotifier {
   ThemeMode get themeMode => _themeMode;
   bool get isDarkMode => _themeMode == ThemeMode.dark;
 
-  void toggleTheme() {
+  ThemeProvider() {
+    _loadTheme();  // Load on startup
+  }
+
+  void toggleTheme() async {
     _themeMode = isDarkMode ? ThemeMode.light : ThemeMode.dark;
     notifyListeners();
+    _saveTheme();  // Save after toggle
+  }
+
+  // Save theme to SharedPreferences
+  void _saveTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkMode', isDarkMode);
+  }
+
+  // Load theme from SharedPreferences
+  void _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedIsDark = prefs.getBool('isDarkMode');
+
+    if (savedIsDark != null) {
+      _themeMode = savedIsDark ? ThemeMode.dark : ThemeMode.light;
+      notifyListeners();
+    }
   }
 }
